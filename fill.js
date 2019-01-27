ce9fe423f0389824ba6b6a5673b5cce880ca0219
Artiom Giza <artiomg@gettaxi.com>
2019-01-27 11:19:32 +0200

    Create fill.js

---
+++ fill.js
@@ -0,0 +1,21 @@
+function fill_times(start_hour, finish_hour) {	
+
+	function generate_time(hour) {	
+		var minutes = "45"
+		if (Math.random() < 0.25) {  minutes = "00"	} else 
+		if (Math.random() < 0.50) {  minutes = "15"	} else 
+		if (Math.random() < 0.75) {  minutes = "30" } 
+
+		return hour + ":" + minutes
+	}
+
+	lines = document.getElementsByTagName("tr")
+
+	for (i = 0; i < lines.length; i++) {
+		var day_type = lines[i].firstElementChild.className
+		if (day_type == "WeekDayHeader") {
+			lines[i].children[2].firstChild.value = generate_time(start_hour)
+			lines[i].children[3].firstChild.value = generate_time(finish_hour)
+		}
+	}
+}
